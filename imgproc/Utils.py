e363d6204d3862754546d31497d1992c28e12aa0
popunbom <fantom0779@gmail.com>
2019-06-25 15:40:28 +0900

    Changed imgproc.Utils - ImageLogger クラス   - テキストログ出力用関数 _logging_msg を追加     - logging_img, logging_json でメッセージを出すように   - 疑似カラー化処理 get_pseudo_colors を追加   - 関数 logging_img     - 疑似カラー化処理を挟み込むように変更

--- imgproc/Utils.py
+++ imgproc/Utils.py
@@ -6,6 +6,7 @@
 
 
 import os
+import time
 from datetime import datetime
 
 import cv2
@@ -158,6 +159,11 @@ class ImageLogger:
         """
         self.timestamp = datetime.now().strftime( self.fmt_timestamp )
         return
+
+    def _logging_msg( self, *args, end="\n" ):
+        msg = "::ImageLogging:: " + ' '.join( [str( e ) for e in args] ) + end
+        sys.stderr.write( msg )
+        sys.stderr.flush()
     
     def __init__( self, base_path, suffix="", prefix="", separator='_', fmt_timestamp="%Y%m%d_%H%M%S" ) -> None:
         """
@@ -241,13 +247,51 @@ class ImageLogger:
         # Regenerate Timestamp
         self._update_timestamp()
         
-        eprint( "::ImageLogger:: created directory -- '{dir_path}'".format(
+        self._logging_msg( "created directory -- '{dir_path}'".format(
             dir_path=self.dir_path
         ) )
         
         return os.mkdir( self.dir_path )
     
-    def logging_img( self, img, file_name, overwrite=False ):
+    def get_psuedo_colors( self, size=1, range_h=(0, 180), range_s=255, range_v=(100, 255) ):
+        TYPE_ASSERT( size, [int, np.sctypes['uint'], np.sctypes['int']] )
+        TYPE_ASSERT( range_h, [tuple, int, np.sctypes['uint'], np.sctypes['int']] )
+        TYPE_ASSERT( range_s, [tuple, int, np.sctypes['uint'], np.sctypes['int']] )
+        TYPE_ASSERT( range_v, [tuple, int, np.sctypes['uint'], np.sctypes['int']] )
+        
+        hsv = list()
+        np.random.seed(int(time.time()))
+        
+        for _range in[range_h, range_s, range_v]:
+            if isinstance(_range, tuple):
+                hsv.append(np.random.randint(low=_range[0], high=_range[1], size=size, dtype=np.uint8))
+            else:
+                hsv.append(np.ones( (size), dtype=np.uint8 ) * _range)
+        
+        hsv = np.stack(hsv, axis=1).reshape(size, 1, 3)
+        
+        rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        
+        return rgb.reshape(size, 3)
+    
+    def get_psuedo_color_img( self, img ):
+        TYPE_ASSERT( img, np.ndarray )
+        
+        
+        psuedo_color_img = np.zeros( (*img.shape[:2], 3), dtype=np.uint8)
+        
+        max = np.max(img)
+        
+        colors = self.get_psuedo_colors(size=max, range_s=(100, 255), range_v=(100, 255))
+        
+        for label_num, color in enumerate(colors):
+            psuedo_color_img[img == label_num] = color
+            
+        return psuedo_color_img
+        
+        
+        
+    def logging_img( self, img, file_name, overwrite=False, do_pseudo_color=False ):
         """
         画像をロギングする処理
         
@@ -298,6 +342,10 @@ class ImageLogger:
         elif isinstance( img, imageMb ):
             img = mamba2cv( img )
         else:
+            # Pseudo colorization
+            if do_pseudo_color:
+                img = self.get_psuedo_color_img(img)
+            
             # Convert data depth
             if img.dtype not in [np.uint8, np.float32]:
                 img = img.astype( np.float32 )
@@ -305,9 +353,16 @@ class ImageLogger:
         
         # Write file
         if img.dtype != np.uint8:
-            return cv2.imwrite( os.path.join( self.dir_path, file_name + ".tiff" ), img )
+            img_file_path = os.path.join( self.dir_path, file_name + ".tiff" )
         else:
-            return cv2.imwrite( os.path.join( self.dir_path, file_name + ".png" ), img )
+            img_file_path = os.path.join( self.dir_path, file_name + ".png" )
+            
+        if cv2.imwrite( img_file_path, img ):
+            self._logging_msg("logging image succesfully ! -- {img_file_path}".format(
+                img_file_path=img_file_path
+            ))
+            
+        return
     
     def logging_json( self, dict_obj, file_name, overwrite=False ):
         """
@@ -370,10 +425,17 @@ class ImageLogger:
             ) )
         
         with open( file_path, "wt" ) as f:
-            return json.dump( dict_obj,
+            result = json.dump( dict_obj,
                               f,
                               ensure_ascii=False,
                               sort_keys=True,
                               indent="\t",
                               default=_jsonize
                               )
+        
+        if result:
+            self._logging_msg("logging json successfully -- {json_file_path}".format(
+                json_file_path=file_path
+            ))
+
+        return
