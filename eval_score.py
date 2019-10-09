b115e79754d361a60b89e6ef02e2143af056be32
popunbom <fantom0779@gmail.com>
2019-10-10 00:11:31 +0900

    Edit: eval_score.py - より汎用性のある実装に変更

--- eval_score.py
+++ eval_score.py
@@ -8,15 +8,30 @@ from os import path
 import cv2
 import numpy as np
 
-ROOT_PATH = "./img/result"
+# RESULT_ROOT_PATH = "./img/result"
+
+RESULT_ROOT_PATH = "./tmp/notebooks/EdgeAngleVariance+HPF"
+GT_ROOT_PATH = "./img/resource/ground_truth"
 
 result = cv2.imread(
-    path.join( ROOT_PATH, "roi1_result.png" )
-)[:, :, 1].astype( bool )
+    path.join( RESULT_ROOT_PATH, "20191009_152809/System - Result(Merged).png" )
+)
 
 gt = cv2.imread(
-    path.join( ROOT_PATH, "roi1_ground_truth.png" )
-)[:, :, 2].astype( bool )
+    path.join( GT_ROOT_PATH, "IMG_6955-qv.png" )
+)
+
+assert result.shape[:2] == gt.shape[:2], \
+    f"""
+    ERROR! -- result must be same size of ground-truth
+    result.shape: {result.shape}
+        gt.shape: {gt.shape}
+    """
+
+for data in [result, gt]:
+    if data.ndim == 3:
+        data = np.any(data > 0, axis=2)
+
 
 TP = np.count_nonzero(
     result & gt
@@ -49,4 +64,4 @@ scores.update(
 
 
 for k, v in scores.items():
-    print(f"{k}: {v:2.3f}")
+    print(f"{k:15s}: {v:2.3f}")
