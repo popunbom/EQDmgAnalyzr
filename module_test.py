92ac180bd55c1a9966d7b3b8976e8d631035b720
popunbom <fantom0779@gmail.com>
2019-11-24 17:06:23 +0900

    Edit: module_test.py - imgproc/edge_line_feature.py の検証

--- module_test.py
+++ module_test.py
@@ -6,62 +6,24 @@
 
 import cv2
 import numpy as np
-from skimage.morphology import disk
 
-from scipy import ndimage as ndi
-
-from imgproc.edge import EdgeProcedures
-from imgproc.hog import HoGFeature
-from imgproc.utils import compute_by_window, zoom_to_img_size
-from utils.mpl import show_images, imshow
+from imgproc.edge_line_feature import EdgeLineFeatures
 from utils.logger import ImageLogger
-from os import path
 
-PATH_ROOT = "./img/resource"
 
 img = cv2.imread(
-    path.join( PATH_ROOT, "aerial_roi1_raw_denoised_clipped_ver2_equalized.png" )
-    # path.join(PATH_ROOT, "aerial_roi2_strong-denoised_clipped_equalized.png")
-    # "/Users/popunbom/Downloads/aerial_roi1_raw_denoised_test.png"
-)
-# X, Y, W, H = 0, 441, 348, 287
-# img = img[Y:Y + H, X:X + W]
-img_gs = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-logger = ImageLogger( "./img/tmp/module_test/hog+edge" )
-
-hog = HoGFeature( img, logger )
-hog.calc_features( feature_vector=False )
-
-var = np.var(
-    hog.features,
-    axis=(2, 3, 4)
+    "img/resource/edge2.bmp",
+    cv2.IMREAD_GRAYSCALE
 )
 
-var = zoom_to_img_size( var / var.max(), img_gs.shape )
-
-logger.logging_img( var, "hog_variance" )
-
-edge = EdgeProcedures( img )
-
-angle_var = edge.get_feature_by_window(
-    edge.angle_variance_using_mean_vector,
-    window_size=8,
-    step=1
+logger = ImageLogger(
+    "./tmp/edge_line_features"
 )
 
-mag_stddev = edge.get_feature_by_window(
-    edge.magnitude_stddev,
-    window_size=8,
-    step=1
+inst = EdgeLineFeatures(
+    img,
+    logger=logger
 )
 
-logger.logging_img( angle_var, "angle_variance" )
-logger.logging_img( mag_stddev, "magnitude_stddev" )
-
-mag_stddev /= mag_stddev.max()
-angle_var /= angle_var.max()
-
-result = angle_var * mag_stddev
-
-logger.logging_img( result, "result" )
+inst.classify_pixel()
+inst.calc_metrics()
