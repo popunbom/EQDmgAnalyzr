37081dc04455d978e1a2903f7c56c71aebc9aa38
popunbom <fantom0779@gmail.com>
2019-11-24 16:53:57 +0900

    Refactored: utils/common.py - utils/assertion.py のリファクタリングに対応 - eprint 関数を修正   - デバッグ用フラッグの追加   - tqdm 内での実行に対応

--- utils/common.py
+++ utils/common.py
@@ -5,12 +5,13 @@
 # Created At: 2019/08/29
 
 import sys
-import inspect
 
-from .exception import CannotFindVariable
+from tqdm import tqdm
+
+from utils.assertion import TYPE_ASSERT
 
 
-def eprint( *args, end="\n" ):
+def eprint( *args, end="\n", debug_flag=None, use_tqdm=False ):
     """
     print 関数の標準エラー出力版
     Parameters
@@ -19,68 +20,26 @@ def eprint( *args, end="\n" ):
         出力したい変数
     end : str, default "\n"
         行末に付加される文字列
+    debug_flag : None or bool
+        デバッグモードかどうか
+        - None または True の場合に出力される
+    use_tqdm : bool
+        tqdm を使用するかどうか
+        - tqdm のループ内で使用する場合は True にする必要がある
 
     Returns
     -------
     """
-    msg = ' '.join( [str( e ) for e in args] ) + end
-    sys.stderr.write( msg )
-    sys.stderr.flush()
-
-
-def get_var_name( var ):
-    """
-    変数から変数名を取得する
-
-    Parameters
-    ----------
-    var : object
-        変数名を取得したい変数
-
-    Returns
-    -------
-    str
-        取得した変数名
-    """
+    TYPE_ASSERT( debug_flag, [None, bool] )
     
-    if var is None:
-        return "None"
-    
-    for fi in reversed( inspect.stack() ):
-        names = [var_name for var_name, var_val in fi.frame.f_locals.items() if var_val is var]
-        if len( names ) > 0:
-            return "'" + names[0] + "'"
-    else:
-        raise CannotFindVariable( "Cannot find variable name" )
-
-
-def get_qualified_class_name( _type ):
-    """
-    オブジェクトの完全クラス名の取得
-
-    Parameters
-    ----------
-    _type : type object
-        取得したいオブジェクトの type オブジェクト
-
-    Returns
-    -------
-    str
-        オブジェクトの完全名
-    """
-    
-    if _type is None:
-        return "None"
-    
-    is_not_builtins = hasattr( _type, "__module__" ) and _type.__module__ != "builtins"
-    
-    if is_not_builtins:
-        return "'{module_name}.{class_name}'".format(
-            module_name=_type.__module__,
-            class_name=_type.__name__
-        )
-    else:
-        return _type.__name__
+    if debug_flag is None or debug_flag:
+        msg = ' '.join( [str( e ) for e in args] )
+        
+        if use_tqdm:
+            tqdm.write( msg, end=end )
+        else:
+            sys.stderr.write( msg + end )
+            sys.stderr.flush()
 
 
 def dec_debug( func ):
